6bf034e1de21fd6283f9be29ce7343f0eb42684d
Stephen Reganato <sreganato@mac.com>
2025-07-22 14:44:25 -0400

    Add basic App.js for Expo entry point

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Hello from Expo CI!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
